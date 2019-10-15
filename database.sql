a5ac364ec0539e922e8e4a5bcbdbd7964a4efb49
Luke Schleder <luke.schleder@gmail.com>
2019-10-15 17:19:37 -0500

    complete the basic questions

--- database.sql
+++ database.sql
@@ -1,9 +1,34 @@
---1How do you get all users from Chicago?
---2How do you get all users with usernames that contain the letter a?
---3The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
---4How do you select all users that have attempted 9 or more transactions?
---5How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
---6How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
---7How do you get all users with account balances that are more than $100?
---8How do you add a new account?
---9The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+--1. How do you get all users from Chicago?
+SELECT * FROM "accounts"
+WHERE "city"='chicago';
+--2. How do you get all users with usernames that contain the letter a?
+SELECT * FROM "accounts"
+WHERE "username" ILIKE '%A%';
+--3. The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
+UPDATE "accounts" SET "account_balance" = '10'
+WHERE "transactions_completed"= '0' AND "transactions_attempted"= '0';
+--4. How do you select all users that have attempted 9 or more transactions?
+SELECT * FROM "accounts"
+WHERE "transactions_attempted" > '9';
+--5. How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+SELECT "username", "account_balance" FROM "accounts"
+ORDER BY "account_balance" DESC
+LIMIT 3;
+--6. How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+SELECT "username", "account_balance" FROM "accounts"
+ORDER BY "account_balance" ASC 
+LIMIT 3;
+--7. How do you get all users with account balances that are more than $100?
+SELECT * FROM "accounts"
+WHERE "account_balance"> 100;
+--8. How do you add a new account?
+INSERT INTO accounts (username, city, transactions_completed, transactions_attempted, account_balance) 
+VALUES ('luke', 'minneapolis', 15, 16, 12000);
+--9. The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
+DELETE FROM "accounts"
+WHERE ("city" ILIKE 'miami' OR "city" ILIKE 'phoenix') AND "transactions_completed"<5;
+
+--10. Anthony moved to Santa Fe.
+--11. Grace closed her account.
+--12. Travis made a withdrawl of $20,000. What's their new balance? NOTE: Research RETURNING
+--13. The Bank needs to track last names. NOTE: Research ALTER TABLE https://www.postgresql.org/docs/10/static/sql-altertable.html

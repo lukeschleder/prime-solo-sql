1999a4376db9815a9dab545035d00a0eb94869b1
Luke Schleder <luke.schleder@gmail.com>
2019-10-15 15:22:16 -0500

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,9 @@
+--1How do you get all users from Chicago?
+--2How do you get all users with usernames that contain the letter a?
+--3The bank is giving a new customer bonus! How do you update all records with an account balance of 0.00 and a transactions_attempted of 0? Give them a new account balance of 10.00.
+--4How do you select all users that have attempted 9 or more transactions?
+--5How do you get the username and account balance of the 3 users with the highest balances, sorted highest to lowest balance? NOTE: Research LIMIT
+--6How do you get the username and account balance of the 3 users with the lowest balances, sorted lowest to highest balance?
+--7How do you get all users with account balances that are more than $100?
+--8How do you add a new account?
+--9The bank is losing money in Miami and Phoenix and needs to unload low transaction customers: How do you delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
